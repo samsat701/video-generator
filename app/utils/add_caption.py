45ca97d790d7de26e1a113f269f3a6bf3c3444d6
samsat701 <145064993+samsat701@users.noreply.github.com>
2024-08-08 01:44:30 -0500

    Fix ImageDraw textsize issue by using textbbox

--- app/utils/add_caption.py
+++ app/utils/add_caption.py
@@ -10,15 +10,17 @@ def add_caption_to_image(image, text):
     words = text.split()
     line = ""
     for word in words:
-        if draw.textsize(line + word, font=font)[0] < width - 20:  # 20 for padding
-            line += word + " "
+        line_with_word = line + word + " "
+        bbox = draw.textbbox((0, 0), line_with_word, font=font)
+        if bbox[2] < width - 20:  # 20 for padding
+            line = line_with_word
         else:
             lines.append(line)
             line = word + " "
     lines.append(line)
 
     # Adjust box height based on the number of lines
-    box_height = (len(lines) * draw.textsize(lines[0], font=font)[1]) + 20  # 20 for padding
+    box_height = (len(lines) * draw.textbbox((0, 0), lines[0], font=font)[3]) + 20  # 20 for padding
     box = [(0, height - box_height), (width, height)]
 
     # Draw the black box
@@ -27,9 +29,9 @@ def add_caption_to_image(image, text):
     # Draw the text on the black box
     text_y = height - box_height + 10  # 10 for padding
     for line in lines:
-        text_size = draw.textsize(line, font=font)
-        text_x = (width - text_size[0]) // 2
+        bbox = draw.textbbox((0, 0), line, font=font)
+        text_x = (width - bbox[2]) // 2
         draw.text((text_x, text_y), line, font=font, fill="white")
-        text_y += draw.textsize(line, font=font)[1]
+        text_y += bbox[3]
 
     return image
