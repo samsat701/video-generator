91f8e74f054fd07a874f9ab7e3e2a32f74cb755e
samsat701 <145064993+samsat701@users.noreply.github.com>
2024-08-08 01:30:25 -0500

    Initial commit

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 from google.generativeai import configure
 
 # Set up your API key for Gemini
-API_KEY = os.getenv('API_KEY', 'your_default_api_key')
+API_KEY = os.getenv('API_KEY', 'AIzaSyA859D0AGFPZPVd5ysBL5KTHLIVsy5rCVg')
 configure(api_key=API_KEY)
