e46d82f2edc743ec7ea2aa5ed29864af5227f712
samsat701 <145064993+samsat701@users.noreply.github.com>
2024-08-08 01:32:41 -0500

    Initial commit

--- app/config.py
+++ app/config.py
@@ -1,6 +1,12 @@
 import os
 from google.generativeai import configure
+from dotenv import load_dotenv
+
+# Load environment variables from .env file, if it exists
+load_dotenv()
 
 # Set up your API key for Gemini
-API_KEY = os.getenv('API_KEY', 'AIzaSyA859D0AGFPZPVd5ysBL5KTHLIVsy5rCVg')
+API_KEY = os.getenv('API_KEY')
+if not API_KEY:
+    raise ValueError("No API key found. Please set the API_KEY environment variable.")
 configure(api_key=API_KEY)
